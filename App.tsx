49e13cc65a31877f9e798b0988e57c9cac9415fb
Jin <jpar303@gmail.com>
2024-02-18 00:02:46 +0900

    added: lottie animation

--- App.tsx
+++ App.tsx
@@ -17,13 +17,9 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import LottieView from 'lottie-react-native';
+
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -76,20 +72,13 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Section title="See Your Changes122" />
+          <LottieView
+            source={require('./assets/lottieFiles/lottie_skateBoard.json')}
+            autoPlay
+            loop
+            style={{width: 100, height: 100}}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
