8fe6c65bf2758012128c2765a7d3b1257a0cbe94
Jin <jpar303@gmail.com>
2024-04-10 17:45:05 +0900

    added: home screen

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -5,7 +6,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -15,15 +16,81 @@ import {
   Text,
   useColorScheme,
   View,
+  Image,
+  TextInput,
 } from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 import LottieView from 'lottie-react-native';
 import useBearStore from './store/dataStore';
-import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
+
+const Header = () => {
+  const [search, setSearch] = useState('');
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        marginTop: 50,
+        padding: 10,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        height: 200,
+        borderWidth: 1,
+        borderColor: 'black',
+        borderStyle: 'solid',
+      }}>
+      <View
+        style={{
+          width: '100%',
+          height: '80%',
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
+          borderWidth: 1,
+          borderColor: 'red',
+          borderStyle: 'solid',
+        }}>
+        <Image
+          style={{
+            width: 120,
+            height: 97,
+          }}
+          source={require('./assets/homeScreen/Brand_Logo.png')}
+        />
+        <Image
+          style={{
+            width: 50,
+            height: 50,
+          }}
+          source={require('./assets/homeScreen/profile_pic.png')}
+        />
+      </View>
+      <View
+        style={{
+          width: '80%',
+          height: '20%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderWidth: 1,
+          borderColor: 'blue',
+          borderStyle: 'solid',
+        }}>
+        <TextInput
+          onChangeText={setSearch}
+          value={search}
+          placeholder="Search"
+        />
+      </View>
+    </View>
+  );
+};
 
 function Section({children, title}: SectionProps): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -57,7 +124,7 @@ function App(): React.JSX.Element {
   const bears = useBearStore((state: any) => state.bears);
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? Colors.black : Colors.white,
   };
 
   return (
@@ -69,11 +136,11 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <Header />
           <Section title="See Your Changes12211" />
           <Text>{bears}</Text>
           <Text style={tw`text-yellow-700`}>Jin1129</Text>
